fe65ff434cd871f6e4ffd64901bf2e0cb65169ef
Vadzim Hardzenia <vadzim_hardzenia@epam.com>
2020-04-27 18:21:18 +0300

    feat: implement dashboard page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/shared/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPageComponent implements OnInit {
 
-  constructor() { }
+  products = []
+  pSub: Subscription
+  rSub: Subscription
+
+  constructor(
+    private productServ: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.pSub = this.productServ.getAll().subscribe(products => {
+      console.log(products)
+      this.products = products
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+
+    if (this.rSub) {
+      this.rSub.unsubscribe()
+    }
+  }
+
+  remove(id) {
+    this.rSub = this.productServ.remove(id).subscribe(() => {
+      this.products = this.products.filter(product => product.id !== id)
+    })
   }
 
 }
